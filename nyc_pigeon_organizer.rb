17306f0e90e83f0aae37d4f12780a119f8ef1e72
Caroline Lee <carolinel2610@gmail.com>
2018-01-18 19:38:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -32,7 +32,7 @@ def nyc_pigeon_organizer(data)
       }
     }
   }
-  binding.pry
+  # binding.pry
   hash
 end
 #
@@ -95,11 +95,11 @@ end
 #     :lives => ["Central Park"]
 #   }
 # }
-
-{"Theo"=>{:color=>[:grey], :gender=>[:male], :lives=>["Subway"]},
- "Peter Jr."=>{:color=>[:grey], :gender=>[:male], :lives=>["Library"]},
- "Lucky"=>{:gender=>[:male], :lives=>["Central Park"]},
- "Ms. K"=>{:color=>[:white], :gender=>[:female], :lives=>["Central Park"]},
- "Queenie"=>{:color=>[:brown], :gender=>[:female], :lives=>["Subway"]},
- "Andrew"=>{:gender=>[:male], :lives=>["City Hall"]},
- "Alex"=>{:color=>[:brown], :gender=>[:male], :lives=>["Central Park"]}}
+#
+# {"Theo"=>{:color=>[:grey], :gender=>[:male], :lives=>["Subway"]},
+#  "Peter Jr."=>{:color=>[:grey], :gender=>[:male], :lives=>["Library"]},
+#  "Lucky"=>{:gender=>[:male], :lives=>["Central Park"]},
+#  "Ms. K"=>{:color=>[:white], :gender=>[:female], :lives=>["Central Park"]},
+#  "Queenie"=>{:color=>[:brown], :gender=>[:female], :lives=>["Subway"]},
+#  "Andrew"=>{:gender=>[:male], :lives=>["City Hall"]},
+#  "Alex"=>{:color=>[:brown], :gender=>[:male], :lives=>["Central Park"]}}
